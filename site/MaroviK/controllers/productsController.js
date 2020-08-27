c9fb780d47a7185999c2d3b91c97483556ccb1b3
garciarosmmery <garciarossmery@gmail.com>
2020-08-27 11:15:58 -0300

    agregué controlador para la vista de los productos por subcategoria

--- site/MaroviK/controllers/productsController.js
+++ site/MaroviK/controllers/productsController.js
@@ -14,6 +14,30 @@ module.exports = {
         res.render('categoria', {
             categoria: dataCategory[0]
         });
+    },
+    view_for_subcategory: (req,res) => {
+        
+        let categoria = req.params.categoria;
+        let subcategoria = req.params.subcategoria;
 
+        let dataCategory = categoriesDataBase.filter(category => {
+            return (category.category == categoria);
+        });
+
+        let dbProductsForCategory = productsDataBase.filter(productCategory => {
+            return (productCategory.category == categoria);
+        });
+
+        let dbProductsForSubcategory = dbProductsForCategory.filter(product => {
+            return (product.subcategory == subcategoria);
+        });
+
+        //res.send(dataCategory);
+        //res.send(dbProductsForSubcategory);
+        res.render('subcategoria', {
+
+            categoria: dataCategory[0],
+            productos: dbProductsForSubcategory
+        });
     }
 }
