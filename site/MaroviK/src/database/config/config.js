48418edd4792adc21dfa8b85fdbc08db73fd8799
Kevin170597 <kevin170597@gmail.com>
2020-10-04 20:34:15 -0300

    .sequelizerc

--- site/MaroviK/src/database/config/config.js
+++ site/MaroviK/src/database/config/config.js
@@ -1,10 +1,9 @@
- module.exports = {
+{
   "development": {
     "username": "root",
     "password": null,
-    "database": "marovik",
+    "database": "database_development",
     "host": "127.0.0.1",
-    "port": "3306",
     "dialect": "mysql"
   },
   "test": {
