2820fc65b530e40fa0d15bad74b7941b63e2ee79
micaelamattia <66717388+micaelamattia@users.noreply.github.com>
2020-10-06 00:17:41 -0300

    agrega modelo Category.js en carpeta database

---
+++ site/MaroviK/src/database/models/Category.js
@@ -0,0 +1,44 @@
+module.exports = (sequelize, DataTypes) =>{
+
+    let alias = 'categories';
+
+    let cols ={
+        id:{
+            type:DataTypes.INTERGER(11),
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+
+
+        title:{
+            type:DataTypes.STRING(45),
+            allowNull: false
+        },
+
+
+        namePath:{
+            type:DataTypes.STRING(45),
+            allowNull: false
+        },
+
+        image:{
+            type:DataTypes.STRING(100),
+            allowNull: false
+        }
+
+
+    }
+
+    let config={
+        tableName: 'categories',
+        timestamps: false
+
+    }
+
+    const Category = sequelize.define(alias, cols, config);
+    return Category
+
+
+
+}
