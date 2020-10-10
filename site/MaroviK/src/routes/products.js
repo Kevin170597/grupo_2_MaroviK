e4f66c17fbf59b2d2ce462504ece3a0dd4370aaf
Kevin170597 <kevin170597@gmail.com>
2020-10-10 17:25:30 -0300

    modifique contorlador y rutas para listar categorias, subcategorias y productos

--- site/MaroviK/src/routes/products.js
+++ site/MaroviK/src/routes/products.js
@@ -19,16 +19,16 @@ const multerProduct = require('../middlewares/multerProduct');
 
 router.get('/add', sessionUserCheck, controller.viewProductAdd);
 router.get('/add/form', sessionUserCheck, controller.viewProductAdd);
-//router.get('/detail/:id', controller.view_product_detail);
-router.get("/detail/:idproduct", controller.ver_producto_detalle);
+router.get("/detail/:idproduct", controller.view_product_detail);
 
 router.get('/show/:id', sessionUserCheck, controller.view_product_show);
 router.put('/edit/:id', multerProduct.any(), sessionUserCheck, controller.update_product);
 router.delete('/delete/:id', sessionUserCheck, controller.delete_product);
 
-router.get("/:categoria/:subcategoria", controller.ver_productos);
+
 router.get('/:categoria', controller.view_for_category);
-//router.get('/:categoria/:subcategoria', controller.view_for_subcategory);
+router.get("/categorias/:subcategoria", controller.view_for_subcategory);
+
 
 router.post('/add/form', multerProduct.any(), sessionUserCheck, controller.publicProduct);
 
