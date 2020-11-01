b4dce83d1ea41bd27150966228c2da4f370bfa86
micaelamattia <66717388+micaelamattia@users.noreply.github.com>
2020-11-01 04:02:40 -0300

    agrega validacion js inicioSesion

--- site/MaroviK/public/javascripts/inicioSesion.js
+++ site/MaroviK/public/javascripts/inicioSesion.js
@@ -1,37 +1,68 @@
+
+
 window.addEventListener('load',function(){
     let loginForm = document.getElementById('loginForm')
     let inputEmail = document.getElementById('email')
     let inputPass = document.getElementById('password')
     let regexEmail = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
 
+    loginForm.addEventListener("submit", (e) =>{
+        let errorEmailMessage = [];
+        let errorEmailFormat = [];
+        let OkEmailMesagge = [];
 
-
-    function loginValitor(){
-        let errores = []
-
-        if(!regexEmail.test(inputEmail.value)){
-            errores.push('Debes ingresar un email valido')
-        }
-        if(!inputPass.value.length < 0){
-            errores.push('Debes ingresar una contraseña')
-        }
-
-        return errores
-
-    }
-
-    loginForm.onsubmit = (e) =>{
-        e.preventDefault()
-
-        let errores = loginValitor()
-
-        if(errores.length > 0){
-
-        }else{
-            loginForm.submit()
+        if(inputEmail.value === "" || inputEmail.value == null){
+            errorEmailMessage.push("Debe ingresar un email.")
+        }else if(inputEmail.value != ""){
+            if(regexEmail.test(inputEmail.value)){
+                email.style.border = "solid 1px green"
+                OkEmailMesagge.push('<i class="far fa-check-circle"></i>')
+                
+            } else{
+                errorEmailFormat.push("El email no es válido.");
+            }
         }
 
-    }
+        if(OkEmailMesagge.length > 0){
+            errorEmail.innerHTML = OkEmailMesagge.join();
+        }
 
+        if(errorEmailMessage.length > 0){
+            e.preventDefault();
+            errorEmail.innerText = errorEmailMessage.join();
+            inputEmail.style.border = "solid 1px  rgb(225, 70, 49)"
+        }
+        if(errorEmailFormat.length > 0){
+            e.preventDefault();
+            errorEmail.innerText = errorEmailFormat.join();
+            inputEmail.style.border = "solid 1px rgb(225, 70, 49)"
+        }
+      ////////////////////////////////////////////////////////////////////////////
+
+        let errorPasswordMessage = [];
+        let OkPasswordMesagge = [];
+
+        if(inputPass.value === "" || inputPass.value == null){
+          errorPasswordMessage.push("Debe ingresar una contraseña.")
+        }else if(inputPass.value != "" && inputPass.value.length > 0){
+          inputPass.style.border = "solid 1px green"
+          OkPasswordMesagge.push('<i class="far fa-check-circle"></i>')
+        }
+
+        if(OkPasswordMesagge.length > 0){
+          errorPassword.innerHTML = OkPasswordMesagge.join();
+        }
+
+        if(errorPasswordMessage.length > 0){
+          e.preventDefault();
+          errorPassword.innerText = errorPasswordMessage.join();
+          password.style.border = "solid 1px  rgb(225, 70, 49)"
+        }
+        if(errorPasswordCharacter.length > 0){
+          e.preventDefault();
+          errorPassword.innerText = errorPasswordCharacter.join();
+          password.style.border = "solid 1px rgb(225, 70, 49)"
+        }
+    })
 
 })
